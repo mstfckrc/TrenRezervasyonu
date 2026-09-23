e0273d0fccd02ea9eb2d565c635b23f0c1cc839c
mstfckrc <mstfckrc277@gmail.com>
2026-09-23 13:58:08 +0300

    Initial Spring Boot project

---
+++ src/main/java/com/mustafa/trenrezervasyonu/TrenRezervasyonuApplication.java
@@ -0,0 +1,13 @@
+package com.mustafa.trenrezervasyonu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrenRezervasyonuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrenRezervasyonuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustafa/trenrezervasyonu/TrenRezervasyonuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafa.trenrezervasyonu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrenRezervasyonuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
